edb833fa287b195ad7228d42147e7539c19aec72
Sjoerd Lohuis <git@initwp.com>
2016-12-08 14:43:25 +0100

    SMACSSed page and single

--- single.php
+++ single.php
@@ -9,8 +9,8 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div id="primary" class="l-primaryContent">
+		<main id="main" class="l-main" role="main">
 
 		<?php
 		while ( have_posts() ) : the_post();
